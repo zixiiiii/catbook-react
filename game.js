a63eb9367bf172b0ba8288bda4beb5f1a52912ae
Sammy Liu <xsl.sammy@gmail.com>
2023-11-15 17:16:40 -0500

    set intervial

--- game.js
+++ game.js
@@ -1,11 +1,15 @@
+const SNAKE_SPEED = 5; //num of times update per sec
+
 const main = () => {
     update();
     draw();
 }
 
-setInterval(main);
+
+setInterval(main, 1000 / SNAKE_SPEED);
 
 const update = () => {
+    console.log('update')
 }
 
 const draw = () => {
